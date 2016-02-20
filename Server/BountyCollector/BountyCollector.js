d7688f27b47d476a0c3600f909f0d80c8ef7de97
Neal Hamilton <nealhamilton92@gmail.com>
2016-02-20 15:22:24 -0500

    bounty collector improved but waiting for alex to fix goal and goals and baseService.

--- Server/BountyCollector/BountyCollector.js
+++ Server/BountyCollector/BountyCollector.js
@@ -1,13 +1,34 @@
-
 "use strict";
+
 var appConfig = require('./app.config.json');
 var schedule = require('node-schedule');
+var Goal = require('./../DataLayer/models/Goal');
+var Goals = require('./../DataLayer/services/Goals');
 
 (function () {
 
     var scheduledJob = schedule.scheduleJob(appConfig.cronTest, function() {
-
         console.log('recurrence rule has been met... ');
+
+        var goalsList = new Goals({
+            "host": appConfig.mongo.host,
+            "port": appConfig.mongo.port,
+            "store": appConfig.mongo.store,
+            "maxRetries": appConfig.mongo.maxRetries
+        });
+
+        goalsList.onConnected = function() {
+            console.log('on connect');
+            goalsList.save(new Goal({}), function(err, res) {
+                if (err) {
+                    console.log(err.message);
+                }
+                console.log('saved...');
+            });
+        };
+        console.log(goalsList);
+
+
     });
 
     // select from goals table
