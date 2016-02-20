7e8ed4e090f2b80e917d6dedb8ad571fd226b6da
Scott Colegrove <scott.colegrove@egov.com>
2016-02-20 05:14:01 -0500

    added initial Auth0 angular scripts

--- Public/scripts/login/LoginApp.js
+++ Public/scripts/login/LoginApp.js
@@ -3,6 +3,19 @@
 
     angular
         .module('DP.Login', [
-            'ngRoute'
-        ]);
+            'ngRoute',
+            'auth0',
+            'angular-storage',
+            'angular-jwt'
+        ])
+        .config(function (authProvider) {
+          authProvider.init({
+            domain: 'koding2015.auth0.com',
+            clientID: 'XyTbnR40o7JGSWPzXwyFSiRlAst3MCT0'
+          });
+        })
+        .run(function(auth) {
+          // This hooks al auth events to check everything as soon as the app starts
+          auth.hookEvents();
+        });
 })();
--- Public/scripts/login/controllers/signIn.controller.js
+++ Public/scripts/login/controllers/signIn.controller.js
@@ -5,8 +5,19 @@
         .module('DP.Login')
         .controller('signInController', signInController);
 
-    signInController.$inject = ['$scope', '$rootScope'];
-    function signInController(scope, rootScope) {
+    signInController.$inject = ['$scope', '$rootScope','$http', 'auth', 'store', '$location'];
+    function signInController(scope, rootScope, http, auth, store, location) {
         var vm = this;
+
+        scope.login = function () {
+            auth.signin({}, function (profile, token) {
+              // Success callback
+              store.set('profile', profile);
+              store.set('token', token);
+              $location.path('/');
+            }, function () {
+              // Error callback
+            });
+          }
     }
 })();
