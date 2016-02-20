59589ad2e9b2761a7aaf167abf2c6ae0fa6c5c8b
Neal Hamilton <nealhamilton92@gmail.com>
2016-02-20 14:40:03 -0500

    bounty collector started and chron jobs running.

--- Server/BountyCollector/BountyCollector.js
+++ Server/BountyCollector/BountyCollector.js
@@ -1,7 +1,22 @@
-/**
- * Created by amhes on 2/20/2016.
- */
 
-/* * * * * * * * * * * * * * * * * * *
- * Working theory: use node-schedule *
- * * * * * * * * * * * * * * * * * * */
+"use strict";
+var appConfig = require('./app.config.json');
+var schedule = require('node-schedule');
+
+(function () {
+
+    var scheduledJob = schedule.scheduleJob(appConfig.cronTest, function() {
+
+        console.log('recurrence rule has been met... ');
+    });
+
+    // select from goals table
+        // if expiration date < new Date().now().getTime();
+
+
+    // create notification
+
+    // call notification service to send notification out via email
+
+
+})();
