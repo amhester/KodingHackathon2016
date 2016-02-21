669b19fbdda84ca197b9f2ca0a3080a041eab2cd
Scott Colegrove <beefmaestro@gmail.com>
2016-02-21 16:20:04 -0500

    added data ito dash panels

--- Public/scripts/main/controllers/dashboard.controller.js
+++ Public/scripts/main/controllers/dashboard.controller.js
@@ -8,5 +8,22 @@
     dashboardController.$inject = ['$scope', '$rootScope'];
     function dashboardController(scope, rootScope) {
         var vm = this;
+
+        var dashPanels = [
+            {
+                id: "1",
+                route: "/profile",
+                imageUrl: "",
+                buttonText: "Profile"
+            },
+            {
+                id: "2",
+                route: "/goals",
+                imageUrl: "",
+                buttonText: "Goals"
+            }
+        ];
+        console.log(dashPanels);
+        vm.dashPanels = dashPanels;
     }
 })();
