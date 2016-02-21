73de868e6a17230c9ff2204ce49407a985d3a717
Neal Hamilton <nealhamilton92@gmail.com>
2016-02-21 07:49:39 -0500

    adding check for goal sttaus of open in bounty collector

--- Server/BountyCollector/BountyCollector.js
+++ Server/BountyCollector/BountyCollector.js
@@ -3,6 +3,7 @@
 var appConfig = require('./../app.config.json');
 var schedule = require('node-schedule');
 var Goals = require('./../DataLayer/services/Goals');
+var Goal = require('./../DataLayer/models/Goals');
 var Notifications = require('./../DataLayer/services/Notifications');
 var Notification = require('./../DataLayer/models/Notification.js');
 var db = require('./../DataLayer/DataRepository');
@@ -17,27 +18,25 @@ var notification = null;
     var scheduledJob = schedule.scheduleJob(appConfig.BountyCollector.cron, function () {
         console.log("Checking goals.");
 
-        //goals.onConnected = function () {
-            db.Goals
-                .query()
-                .find().toArray(function (err, results) {
-                if (err) {
-                    console.log(err.message);
-                }
-                var now = new Date().getTime();
-                results.forEach(function (obj) {
-                    if (obj.expiration < now) {
+        db.Goals
+            .query()
+            .find().toArray(function (err, results) {
+            if (err) {
+                console.log(err.message);
+            }
+            var now = new Date().getTime();
+            results.forEach(function (obj) {
+                if (obj.expiration < now) {
+                    if (obj.status = Goal.GOAL_STATUSES.OPEN) {
+                        obj.status = Goal.GOAL_STATUSES.EXPIRED;
 
-                        //notification = new Notification({
-                        //    accountId: obj.accountId,
-                        //    seen: false,
-                        //    message: Notification.NOTIFICATION_TYPES.EXPIRED,
-                        //    link: "", // Where do we generate this?
-                        //    email: "nealhamilton92@gmail.com"
-                        //});
-                        // TODO: Make link the real link.
+                        // TODO: generate actual link
                         let link = "/act/" + obj.id;
-                        notification = Notification.fromGoal(obj, Notification.NOTIFICATION_TYPES.EXPIRED, false, `Hey...pay up. <a href="$(link)">See goal</a>`, link);
+
+                        notification = Notification.fromGoal(obj,
+                            Notification.NOTIFICATION_TYPES.EXPIRED,
+                            false,
+                            `Hey...pay up. <a href="$(link)">See goal</a>`, link);
 
                         notifications.save(notification, function(err, notif) {
                             if (err) {
@@ -45,23 +44,23 @@ var notification = null;
                             }
                             console.log(notif);
                             request({
-                                method: 'POST',
-                                uri: appConfig.BountyCollector.notificationUrl,
-                                json: notif
-                            },
-                            function (err, response, body) {
-                                if (err) {
-                                    console.log(err.message);
-                                } else {
-                                    console.log('response:', body);
-                                }
-                            });
+                                    method: 'POST',
+                                    uri: appConfig.BountyCollector.notificationUrl,
+                                    json: notif
+                                },
+                                function (err, response, body) {
+                                    if (err) {
+                                        console.log(err.message);
+                                    } else {
+                                        console.log('response:', body);
+                                    }
+                                });
                         });
-                    } else {
-                        console.log("not expired...");
                     }
-                });
+                } else {
+                    console.log("not expired...");
+                }
             });
-        //};
+        });
     });
 })();
