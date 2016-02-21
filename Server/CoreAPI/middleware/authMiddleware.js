c727ca2f8e5c317b990c7cdc4fa74a833c467101
Neal Hamilton <nealhamilton92@gmail.com>
2016-02-21 12:43:13 -0500

    goal failed

--- Server/CoreAPI/middleware/authMiddleware.js
+++ Server/CoreAPI/middleware/authMiddleware.js
@@ -7,14 +7,10 @@ module.exports.authWall = function (req, res, next) {
     let authHeaderToken = req.header('x-winwin-token');
     //var authHeaderToken = authHeader && authHeader.split(' ').length > 1 ? authHeader.split(' ')[1] : '';
     if (req.url == '/token') {
-        console.log('authwall /token');
         next();
     } else if (req.method == 'POST' && req.url == '/account') {
-        console.log('authwall /account');
         next();
     } else if (authHeaderToken) {
-        console.log('authwall authHeaderToken', authHeaderToken);
-
         jwt.verify(authHeaderToken, config.appSecret, function (err, payload) {
             if(err) {
                 res.send(403, "Unauthorized Access.");
