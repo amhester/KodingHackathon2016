90633de8b5bbd4f7bb717705434f59fba42eb18f
Neal Hamilton <nealhamilton92@gmail.com>
2016-02-21 17:52:58 -0500

    getting expired cards showing properly

--- Server/BountyCollector/BountyCollector.js
+++ Server/BountyCollector/BountyCollector.js
@@ -18,51 +18,53 @@ var notification = null;
     var DogTheBountyHunter = schedule.scheduleJob(appConfig.BountyCollector.cron, function () {
 
         let g = null;
-        if ((g = db.Goals) != null) {
-            g.query()
-                .find().toArray(function (err, results) {
-                if (err) {
-                    console.log('erroring on find().toArray()');
-                    console.log(err.message);
-                }
-                var now = new Date().getTime();
-                results.forEach(function (obj) {
-                    if (obj.expiration < now) {
-                        if (obj.status = Goal.GOAL_STATUSES.OPEN) {
-                            obj.status = Goal.GOAL_STATUSES.EXPIRED;
-                            db.Goals.save(new Goal(obj), function (err, goal) {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-
-                            // TODO: generate actual link
-                            let link = "https://169.44.62.169/goal/" + obj.id + "/expired";
-
-                            notification = Notification.fromGoal(obj,
-                                Notification.NOTIFICATION_TYPES.EXPIRED,
-                                false,
-                                'Hey...pay up. <a href="' + link + '">See goal</a>', link);
-
-                            db.Notifications.save(notification, function (err, notif) {
-                                if (err) {
-                                    console.log(err.message);
-                                }
-                                request({
-                                        method: 'POST',
-                                        uri: appConfig.BountyCollector.notificationUrl,
-                                        json: notif.toJson()
-                                    },
-                                    function (err, response, body) {
-                                        if (err) {
-                                            console.log(err.message);
-                                        }
-                                    });
-                            });
-                        }
+        //if ((g = db.Goals) != null) {
+            db.Goals.onConnected(function() {
+                g.query()
+                    .find().toArray(function (err, results) {
+                    if (err) {
+                        console.log('erroring on find().toArray()');
+                        console.log(err.message);
                     }
+                    var now = new Date().getTime();
+                    results.forEach(function (obj) {
+                        if (obj.expiration < now) {
+                            if (obj.status = Goal.GOAL_STATUSES.OPEN) {
+                                obj.status = Goal.GOAL_STATUSES.EXPIRED;
+                                db.Goals.save(new Goal(obj), function (err, goal) {
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                                });
+
+                                // TODO: generate actual link
+                                let link = "https://169.44.62.169/goal/" + obj.id + "/expired";
+
+                                notification = Notification.fromGoal(obj,
+                                    Notification.NOTIFICATION_TYPES.EXPIRED,
+                                    false,
+                                    'Hey...pay up. <a href="' + link + '">See goal</a>', link);
+
+                                db.Notifications.save(notification, function (err, notif) {
+                                    if (err) {
+                                        console.log(err.message);
+                                    }
+                                    request({
+                                            method: 'POST',
+                                            uri: appConfig.BountyCollector.notificationUrl,
+                                            json: notif.toJson()
+                                        },
+                                        function (err, response, body) {
+                                            if (err) {
+                                                console.log(err.message);
+                                            }
+                                        });
+                                });
+                            }
+                        }
+                    });
                 });
-            });
-        } // else db.Goals is null
+            }
+       // } // else db.Goals is null
     });
 })();
