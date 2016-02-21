05067dbc9817b01fe02cd5685dcb29e10f964725
djragsdale <djragsdale@anderson.edu>
2016-02-20 19:53:48 -0500

    Add goal to seed.js

--- seed.js
+++ seed.js
@@ -4,7 +4,6 @@ const config = require('./Server/CoreAPI/app.config.json');
 const Account = require('./Server/DataLayer/models/Account');
 const Accounts = require('./Server/DataLayer/services/Accounts');
 const _accounts = new Accounts(config.mongo);
-const _goals = new Goals(config.mongo);
 const argv = require('yargs').argv;
 
 if(argv.createDemoAccount) {
@@ -16,33 +15,9 @@ if(argv.createDemoAccount) {
         defaultCharity: 1
     });
 
-    let goalModel = new Goal({
-        id: 0,
-        accountId: 1,
-        name: "HHHHEEEEEYYYY",
-        description: "This isn't cool.",
-        bounty: "7",
-        charityId: 0,
-        expiration: new Date(Date.now()).setYear(2015),
-        status: "B",
-        createdOn: new Date(Date.now()),
-        updatedOn: new Date(Date.now())
-    });
-
     _accounts.onConnected = function () {
         _accounts.save(model, function (err, result) {
             if(err) {
-                console.log(err.message);
-            } else {
-                console.log(result);
-            }
-            process.exit(1);
-        });
-    };
-
-    _goals.onConnected = function () {
-        _goals.save(goalModel, function (err, result) {
-            if (err) {
                 console.log(err.message);
             } else {
                 console.log(result);
